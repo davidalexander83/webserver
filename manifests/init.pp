c5530039a550030791b29223a3e6a965806a0a21
David Alexander <david.alexander@puppet.com>
2018-10-05 04:12:50 +0000

    add init.pp with files

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,12 @@
 class webserver {
   # this module will manage my web server
 
-  
+  file { "C:\\Inetpub\\website":
+    ensure => directory,
+  }
+
+  file { "C:\\Inetpub\\website\\index.html":
+    ensure => file,
+    source => 'puppet:///modules/webserver/index.html',
+  }
 }
