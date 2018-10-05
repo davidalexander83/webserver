f512452ae9b3a47665e2d56d541ecda27cd5e363
David Alexander <david.alexander@puppet.com>
2018-10-05 04:45:01 +0000

    create webserver!

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,8 +15,31 @@ class webserver {
     ensure => directory,
   }
 
-  file { "C:\\Inetpub\\website\\index.html":
+  file { 'index':
     ensure => file,
+    path   => "C:\\Inetpub\\website\\index.html",
     source => 'puppet:///modules/webserver/index.html',
+  }
+
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
+
+  iis_feature { $iis_features:
+    ensure => present,
+  }
+
+  # Delete the default website to prevent a port binding conflict.
+  iis_site { 'Default Web Site':
+    ensure  => absent,
+    require => Iis_feature['Web-WebServer'],
+  }
+
+  iis_site { 'website':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\website',
+    applicationpool => 'DefaultAppPool',
+    require         => [
+      File['index'],
+      Iis_site['Default Web Site']
+    ],
   }
 }
