d8b317e628299b18bfdafef43bb4ee3edbf29df9
David Alexander <david.alexander@puppet.com>
2018-10-05 05:36:53 +0000

    fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,10 @@
 class webserver(
   Hash $websites,
 ){
-
-  $folders = ["C:\\Inetpub"]
-
-  file { 'C:/Inetpub':
-    ensure => directory,
-  }
-
   $iis_features = ['Web-WebServer','Web-Scripting-Tools']
-
   iis_feature { $iis_features:
     ensure => present,
   }
-
-  # Delete the default website to prevent a port binding conflict.
-  iis_site { 'website':
-    ensure  => absent,
-    require => Iis_feature['Web-WebServer'],
-  }
-
 $websites.each |String $website, Hash $attributes| {
     webserver::website { $website:
       * => $attributes,
